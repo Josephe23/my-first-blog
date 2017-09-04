cb36c6c976d8190fbb33b39e5b6c7e934db01dae
Josephe23 <josep19961@hotmail.com>
2017-09-03 20:05:37 -0600

    Cambie el HTML para la pagina.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '43+^(*wqjre(5$!(lq1pj0lphudka2o00y2j4_5+r744wo6@%)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['josep1996.pythonanywhere.com']
 
 
 # Application definition
