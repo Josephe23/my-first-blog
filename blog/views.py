db7238d74d0516cc9311603ab550b431c458be9f
Josephe23 <josep19961@hotmail.com>
2017-09-03 20:36:34 -0600

    Modifique html y views

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from .models import Publicacion
 
 def post_list(request):
     posts = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'Publicaion': posts})
