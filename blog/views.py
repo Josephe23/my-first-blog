3711c50c62c3cf9a8e7d55d05fa9c7703cef64e2
Josephe23 <josep19961@hotmail.com>
2017-09-19 10:58:42 -0600

    Arreglando problemas

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
 
 def post_list(request):
-    posts = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    posts = Publicacion.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    posts = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/post_detail.html', {'posts': posts})
